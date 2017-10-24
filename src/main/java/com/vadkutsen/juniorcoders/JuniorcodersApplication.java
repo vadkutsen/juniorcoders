70c2bf8db48d034ead4812280c3c7edbd45d2b3d
vadkutsen <vadkutsen@gmail.com>
2017-10-24 18:43:41 +0200

    Initial commit for my first Spring boot project

---
+++ src/main/java/com/vadkutsen/juniorcoders/JuniorcodersApplication.java
@@ -0,0 +1,12 @@
+package com.vadkutsen.juniorcoders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorcodersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorcodersApplication.class, args);
+	}
+}
