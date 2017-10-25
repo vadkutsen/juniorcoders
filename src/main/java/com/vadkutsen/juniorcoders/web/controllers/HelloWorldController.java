4eac2fa8b7d916d49f8e6e36a611ddfa6a37c6f6
vadkutsen <vadkutsen@gmail.com>
2017-10-25 03:39:28 +0200

    Added bootstrap and jquery dependencies to the pom.xml file

--- src/main/java/com/vadkutsen/juniorcoders/web/controllers/HelloWorldController.java
+++ src/main/java/com/vadkutsen/juniorcoders/web/controllers/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.vadkutsen.juniorcoders.controllers;
+package com.vadkutsen.juniorcoders.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
