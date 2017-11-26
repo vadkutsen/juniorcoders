1111d31553f84205f3e5c36bd56e38a921a83653
vadkutsen <vadkutsen@gmail.com>
2017-11-26 04:48:48 +0100

    Added feedback form to the Contact us tab

--- src/main/resources/static/js/juniorcoders.js
+++ src/main/resources/static/js/juniorcoders.js
@@ -1,13 +1,10 @@
 $( document ).ready( main );
 
 function main() {
-	
-	$( '.btn-collapse' ).click(function(e) {
-		e.preventDafault();
-		var $this = $(this);
-		var $collapse = $this.closest('.collapse-group').find('.collapse');
-		$collapse.collapse('toggle');
-	});
-	
-	
-}
+
+    $('.btn-collapse').click(function(e) {
+        e.preventDefault();
+        var $this = $(this);
+        var $collapse = $this.closest('.collapse-group').find('.collapse');
+        $collapse.collapse('toggle');
+    });
