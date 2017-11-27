33693a388c308b4edfc7747df9f5aa8a4ea25def
vadkutsen <vadkutsen@gmail.com>
2017-11-27 17:38:37 +0100

    Added production mailer

--- src/main/java/com/vadkutsen/juniorcoders/config/ProductionConfig.java
+++ src/main/java/com/vadkutsen/juniorcoders/config/ProductionConfig.java
@@ -1,12 +1,20 @@
 package com.vadkutsen.juniorcoders.config;
 
+import com.vadkutsen.juniorcoders.backend.service.EmailService;
+import com.vadkutsen.juniorcoders.backend.service.MockEmailService;
+import com.vadkutsen.juniorcoders.backend.service.SmtpEmailService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Profile("prod")
-@PropertySource("file:///${user.home}/.spring/application-prod.properties")
+@PropertySource("file:///${user.home}/spring/application-prod.properties")
 public class ProductionConfig {
 
+    @Bean
+    public EmailService emailService() {
+        return new SmtpEmailService();
+    }
 }
