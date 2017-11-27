c824486eb8297a32d0cf219abec17d456df0a5a7
vadkutsen <vadkutsen@gmail.com>
2017-11-27 03:51:27 +0100

    Fixed some issues

--- src/main/java/com/vadkutsen/juniorcoders/backend/service/AbstractEmailService.java
+++ src/main/java/com/vadkutsen/juniorcoders/backend/service/AbstractEmailService.java
@@ -1,6 +1,13 @@
 package com.vadkutsen.juniorcoders.backend.service;
 
+import com.vadkutsen.juniorcoders.web.domain.frontend.FeedbackPojo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+
 public abstract class AbstractEmailService implements EmailService {
+
+	@Value("${default.to.address}")
+	private String defaultToAddress;
 
 	/**
 	 * Creates a Simple Mail Message from a Feedback Pojo.
@@ -8,6 +15,17 @@ public abstract class AbstractEmailService implements EmailService {
 	 * @return
 	 */
 	protected SimpleMailMessage prepareSimpleMailMessageFromFeedbackPojo(FeedbackPojo feedback) {
-		return null; // for now
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(defaultToAddress);
+		message.setFrom(feedback.getEmail());
+		message.setSubject("[Junior Coders]: Feedback received from " + feedback.getFirstName() + " " + feedback
+				.getLastName() + "!");
+		message.setText(feedback.getFeedback());
+		return message;
+	}
+
+	@Override
+	public void sendFeedbackEmail(FeedbackPojo feedbackPojo) {
+		sendGenericEmailMessage(prepareSimpleMailMessageFromFeedbackPojo(feedbackPojo));
 	}
 }
--- src/main/java/com/vadkutsen/juniorcoders/config/DevelopmentConfig.java
+++ src/main/java/com/vadkutsen/juniorcoders/config/DevelopmentConfig.java
@@ -1,12 +1,20 @@
 package com.vadkutsen.juniorcoders.config;
 
+import com.vadkutsen.juniorcoders.backend.service.EmailService;
+import com.vadkutsen.juniorcoders.backend.service.MockEmailService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Profile("dev")
-@PropertySource("file:///${user.home}/.spring/application-dev.properties")
+@PropertySource("file:///${user.home}/spring/application-dev.properties")
 public class DevelopmentConfig {
-	
+
+
+    @Bean
+    public EmailService emailService() {
+        return new MockEmailService();
+    }
 }
